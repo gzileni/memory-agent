5853d75f65196db6cc10eaef9d922d38b2c81001
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2025-07-26 18:37:00 +0200

    fix: Improve deploy script by adding verbose output for twine upload

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo ">>> Rimozione della cartella dist/"
+rm -rf dist/
+
 echo ">>> Inizio del deploy su PyPI"
 python -m build
 
 echo ">>> Upload su PyPI"
 # Crea il branch se non esiste
-twine upload dist/*
+twine upload dist/* --verbose
